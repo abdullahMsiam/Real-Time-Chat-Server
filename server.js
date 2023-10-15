87b7b9f07d9727c10057f3a2875c4c363889814a
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-10-16 04:06:47 +0600

    chat models

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const { chats } = require('./fake-data/fake-data');
 
 const app = express();
+
+//middleware 
+app.use(cors());
+app.use(express.json());
+
 
 app.get("/", (req, res) => {
     res.send("Hello from server")
 })
-app.listen(5000, () => {
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+})
+
+app.get("/api/chat/:id", (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find(c => c._id === req.params.id);
+    res.send(singleChat);
+})
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
     console.log("server is running")
 })
